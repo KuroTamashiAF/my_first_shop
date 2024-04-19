da4863e861da851562172175dcfe50993a7b1995
Fedorov Alexandr <chughun777akk@mail.ru>
2024-04-19 11:50:42 +0700

    after adding fixtures in db

--- goods/models.py
+++ goods/models.py
@@ -19,7 +19,7 @@ class Categories(models.Model):
 
 class Products(models.Model):
     name = models.CharField(
-        max_length=150, unique=True, verbose_name="Название категории"
+        max_length=150, unique=True, verbose_name="Название продукта"
     )
     slug = models.SlugField(
         max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
@@ -32,7 +32,7 @@ class Products(models.Model):
         default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена"
     )
     discount = models.DecimalField(
-        default=0.00, max_digits=7, decimal_places=2, verbose_name="Скидка в %"
+        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в %"
     )
     quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name="Категория")
