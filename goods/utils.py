fef6a29b625127d9a64afdd834bde44fbdec97cd
Fedorov Alexandr <chughun777akk@mail.ru>
2024-05-29 16:16:48 +0700

    Installing postgresSQL and full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,21 +1,23 @@
 from goods.models import Products
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def q_search(querry):
-    if querry.isdigit() and len(querry)<=5:
+    if querry.isdigit() and len(querry) <= 5:
         return Products.objects.filter(id=int(querry))
-    
-    keywords = [word for word in querry.split() if len(word) >2 ]
 
-    q_objects = Q()
+    vector = SearchVector("name", "description")
+    query = SearchQuery(querry)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("rank")
 
+    # keywords = [word for word in querry.split() if len(word) > 2]
 
-    return Products.objects.filter(q_objects)
+    # q_objects = Q()
 
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
 
-
+    # return Products.objects.filter(q_objects)
--- goods/views.py
+++ goods/views.py
@@ -11,27 +11,24 @@ def catalog(request, category_slug=None):
     page = request.GET.get("page", 1)
     on_sale = request.GET.get("on_sale", None)
     order_by = request.GET.get("order_by", None)
-    query=request.GET.get("q", None)
-
+    query = request.GET.get("q", None)
 
     if category_slug == "all":
         goods = Products.objects.all()
     elif query:
         goods = q_search(query)
-        
+
     else:
         goods = Products.objects.filter(category__slug=category_slug)
-
 
     if on_sale:
         goods = goods.filter(discount__gt=0)
 
-    if order_by and order_by !="default":
-        goods=goods.order_by(order_by)
+    if order_by and order_by != "default":
+        goods = goods.order_by(order_by)
 
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
-
 
     context = {
         "title": "Home - каталог",
@@ -40,7 +37,6 @@ def catalog(request, category_slug=None):
     }
 
     return render(request, "goods/catalog.html", context)
-
 
 
 def product(request, product_slug):
