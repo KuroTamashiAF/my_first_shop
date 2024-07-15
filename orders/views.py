f80120bec81d1b1d7948b99d7d2815a6c37524c8
Alex <chugun777akk@mail.ru>
2024-07-15 08:10:16 +0700

    Create order template and form prepared

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,36 @@
+from django.db import transaction
 from django.shortcuts import render
-from django.template import context
+from orders.forms import OrderCreatedForm
 
 
 # Create your views here.
 def create_order(request):
-    context = {}
+    if request.method == "POST":
+        form = OrderCreatedForm(data=request.POST)
+        if form.is_valid():
+            try:
+                with transaction.atomic():
+                    ...
+
+
+
+
+
+
+
+    
+        
+        else:
+            initial={
+                "first_name": request.user.first_name,
+                "last_name": request.user.last_name,
+
+            }
+            form = OrderCreatedForm(initial=initial)
+
+        context={
+            "title":"Оформление заказа",
+            "form": form,
+        }
+
     return render(request, "orders/create_order.html", context)
