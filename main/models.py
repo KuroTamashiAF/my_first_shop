9e84b3e6614ecb343f7bb32aabdaeedd3773a167
Федоров Александр Андреевич <A.Fedorov@henconru.local>
2024-11-01 10:31:32 +0700

    Make page ' about '

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ class FeedBackCall(models.Model):
     name = models.CharField(max_length=100, verbose_name="Имя абонента")
     phone_number = models.CharField(max_length=15, verbose_name="Номер телефона")
     time_created = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    status = models.BooleanField(verbose_name="Отметка о выполнении", default=False)
 
     def __str__(self):
 
